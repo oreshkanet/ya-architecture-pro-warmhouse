f5ce9ae947814e9190b28f0badd2c7e2a0a116ff
Denis Saygin <oreshka.net@gmail.com>
2025-10-05 07:18:47 +0300

    Приложение Temperature-API

---
+++ apps/temperature_api/internal/delivery/http/dto.go
@@ -0,0 +1,14 @@
+package http
+
+import "time"
+
+type TemperatureResponse struct {
+	Value       float64   `json:"value"`
+	Unit        string    `json:"unit"`
+	Timestamp   time.Time `json:"timestamp"`
+	Location    string    `json:"location"`
+	Status      string    `json:"status"`
+	SensorID    string    `json:"sensor_id"`
+	SensorType  string    `json:"sensor_type"`
+	Description string    `json:"description"`
+}
